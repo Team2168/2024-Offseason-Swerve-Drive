4b0c6f20db645ec53d731287dcec719ab9202b07
Jio Sookhai <jiovannizsook@grotonk12.org>
2024-10-14 16:45:10 -0700

    motion magic, velocity voltage, and cancoder configs finished (values aren't tailored).

--- 2024-Offseason-Swerve-Drive/src/main/java/frc/robot/subsystems/Module.java
+++ 2024-Offseason-Swerve-Drive/src/main/java/frc/robot/subsystems/Module.java
@@ -4,14 +4,17 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.configs.VoltageConfigs;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
@@ -35,14 +38,22 @@ public class Module extends SubsystemBase {
   private Slot0Configs driveSlot0Configs;
   private Slot0Configs azimuthSlot0Configs;
   private MotionMagicConfigs azimuthMagicConfigs;
-  private MotionMagicVoltage azimuthVoltageConfigs;
+  private MotionMagicVoltage azimuthVoltage;
+  private VelocityVoltage driveMotorVelocity;
+  private VoltageConfigs driveMotorVelocityConfigs;
+  private FeedbackConfigs azimuthFeedback;
   private DutyCycleOut driveCycleOut = new DutyCycleOut(0.0);
-  private 
   private double deadband = 0.002;
+  private double drivekP = 0.05;
+  private double drivekI = 0.0025;
+  private double drivekD = 0.005;
+  private double azimuthkP = 0.05;
+  private double azimuthkI = 0.0025;
+  private double azimuthkD = 0.005;
 
-  //TODO
-  //pid, connect cancoder to configs, motoroutputconfigs, motionmagic, feedback.
-  
+  // TODO
+  // pid, connect cancoder to configs, motoroutputconfigs, motionmagic, feedback.
+
   public Module(TalonFX driveMotor, TalonFX azimuthMotor, CANcoder azimuthEncoder) {
     this.driveMotor = driveMotor;
     this.azimuthMotor = azimuthMotor;
@@ -51,12 +62,22 @@ public class Module extends SubsystemBase {
     driveConfiguration = new TalonFXConfiguration();
     azimuthConfiguration = new TalonFXConfiguration();
 
+    azimuthVoltage = new MotionMagicVoltage(0.0);
+    azimuthMagicConfigs = azimuthConfiguration.MotionMagic;
+    azimuthMagicConfigs.withMotionMagicAcceleration(0.0025);
+    azimuthMagicConfigs.withMotionMagicCruiseVelocity(0.01);
+
+    driveMotorVelocity = new VelocityVoltage(0.0);
+    driveMotorVelocityConfigs = driveConfiguration.Voltage;
+    driveMotorVelocityConfigs.withPeakForwardVoltage(10);
+    driveMotorVelocityConfigs.withPeakReverseVoltage(10);
+
     driveMotorOutput = driveConfiguration.MotorOutput;
     azimuthMotorOutput = azimuthConfiguration.MotorOutput;
     driveSlot0Configs = driveConfiguration.Slot0;
     azimuthSlot0Configs = azimuthConfiguration.Slot0;
     azimuthMagicConfigs = azimuthConfiguration.MotionMagic;
-
+    azimuthFeedback = azimuthConfiguration.Feedback;
 
     driveMotorOutput.withInverted(invert);
     driveMotorOutput.withNeutralMode(brake);
@@ -65,7 +86,16 @@ public class Module extends SubsystemBase {
     azimuthMotorOutput.withNeutralMode(brake);
     azimuthMotorOutput.withDutyCycleNeutralDeadband(deadband);
 
-  } 
+    driveSlot0Configs.withKP(drivekP);
+    driveSlot0Configs.withKI(drivekI);
+    driveSlot0Configs.withKD(drivekD);
+    azimuthSlot0Configs.withKP(azimuthkP);
+    azimuthSlot0Configs.withKI(azimuthkI);
+    azimuthSlot0Configs.withKD(azimuthkD);
+
+    azimuthFeedback.withRemoteCANcoder(azimuthEncoder);
+
+  }
 
   @Override
   public void periodic() {
